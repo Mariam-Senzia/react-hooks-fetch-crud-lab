d55532fe3c11281d68eb66a4247b720a772eba46
Mariam Senzia <mariamsenzia@gmail.com>
2024-02-10 14:39:18 +0300

    modified files to handle crud operations

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -9,15 +9,37 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleCorrectAnswer() {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method:"PATCH",
+      headers: {
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify({
+        "correctIndex": correctIndex
+      })
+      .then((res) => res.json())
+      .then((ans) =>console.log(ans))
+    })
+    }
+
+  function handleDelete () {
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+    })
+    .then((res) => res.json())
+    .then((deletequest) => console.log(deletequest))
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleCorrectAnswer} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+
 
 function QuestionList() {
+  const[question,setQuestion] = useState([]);
+
+  const questionList = question.map((quiz) => {
+     return <li key={quiz.prompt}>{quiz.prompt}</li>;
+  })
+
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((ques) => setQuestion(ques))
+  },[])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionList}
+      </ul>
     </section>
   );
 }
